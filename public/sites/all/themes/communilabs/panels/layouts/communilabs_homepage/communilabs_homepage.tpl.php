d11255207ed0641e78ee9c579c176c5f8b908e1b
Rik Barwick <rik@creativitea.co.uk>
2014-05-20 16:53:22 +0100

    few updates

---
+++ public/sites/all/themes/communilabs/panels/layouts/communilabs_homepage/communilabs_homepage.tpl.php
@@ -0,0 +1,48 @@
+<div class="panel-display omega-grid communilabs_homepage" <?php if (!empty($css_id)) { print "id=\"$css_id\""; } ?>>
+  <div class="panel-panel grid-12">
+    <div class="inside"><?php print $content['header']; ?></div>
+  </div>
+  <div class="homepage_wrapper clear">
+	  <div class="panel-panel grid-4 center-content">
+	    <div class="inside"><?php print $content['1']; ?></div>
+	  </div>
+	  <div class="panel-panel grid-4 center-content">
+	    <div class="inside"><?php print $content['2']; ?></div>
+	  </div>
+	  <div class="panel-panel grid-4 center-content">
+	    <div class="inside"><?php print $content['3']; ?></div>
+	  </div>
+  </div>
+  <div class="panel-panel grid-12">
+    <div class="inside"><?php print $content['4']; ?></div>
+  </div>
+  <div class="homepage_wrapper clear">
+	  <div class="panel-panel grid-3">
+	    <div class="inside"><?php print $content['5']; ?></div>
+	  </div>
+	  <div class="panel-panel grid-3">
+	    <div class="inside"><?php print $content['6']; ?></div>
+	  </div>
+	  <div class="panel-panel grid-3">
+	    <div class="inside"><?php print $content['7']; ?></div>
+	  </div>
+	  <div class="panel-panel grid-3">
+	    <div class="inside"><?php print $content['8']; ?></div>
+	  </div>
+  </div>
+  <div class="panel-panel grid-6">
+    <div class="inside"><?php print $content['9']; ?></div>
+  </div>
+  <div class="panel-panel grid-6">
+    <div class="inside"><?php print $content['10']; ?></div>
+  </div>
+  <div class="panel-panel grid-12">
+    <div class="inside"><?php print $content['11']; ?></div>
+  </div>
+  <div class="panel-panel grid-6">
+    <div class="inside"><?php print $content['12']; ?></div>
+  </div>
+  <div class="panel-panel grid-6">
+    <div class="inside"><?php print $content['13']; ?></div>
+  </div>
+</div>
