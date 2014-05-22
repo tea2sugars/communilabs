4e7ac26a579a0b36da6d9e3a42d5eab2b8ffe308
Rik Barwick <rik@creativitea.co.uk>
2014-05-22 16:52:21 +0100

    style for news ticker and adjustments

--- public/sites/all/themes/communilabs/panels/layouts/communilabs_homepage/communilabs_homepage.tpl.php
+++ public/sites/all/themes/communilabs/panels/layouts/communilabs_homepage/communilabs_homepage.tpl.php
@@ -1,19 +1,26 @@
 <div class="panel-display omega-grid communilabs_homepage" <?php if (!empty($css_id)) { print "id=\"$css_id\""; } ?>>
-<div class="homepage_wrapper clear">
+
+<div class="clear">
   <div class="panel-panel grid-12 clear">
     <div class="inside"><?php print $content['header']; ?></div>
-  </div>
+  </div> 		
 </div>
-  <div class="homepage_wrapper clear" id="news-ticker">
-  	<div class="wrapper-inside clear">
-  		<div class="panel-panel grid-8">
-	    	<div class="inside"><?php print $content['news-1']; ?></div>
+
+<div class="clear" id="news-ticker">
+	<div class="inside">
+		<div class="panel-panel grid-8">
+	    	<div class="inside">
+	    		<?php print $content['news-1']; ?>
+	    	</div>
 		</div>
 		<div class="panel-panel grid-4">
-	   		<div class="inside"><?php print $content['news-2']; ?></div>
-		</div>
-  	</div>
-  </div>
+	   		<div class="inside">
+	   			<?php print $content['news-2']; ?>
+	   		</div>
+	   	</div>
+	</div>
+</div>
+  
   <div class="homepage_wrapper clear">
 	  <div class="panel-panel grid-4 center-content">
 	    <div class="inside"><?php print $content['1']; ?></div>
@@ -25,8 +32,10 @@
 	    <div class="inside"><?php print $content['3']; ?></div>
 	  </div>
   </div>
-  <div class="panel-panel grid-12">
-    <div class="inside"><?php print $content['4']; ?></div>
+  <div class="homepage_wrapper clear">
+	  <div class="panel-panel grid-12">
+	    <div class="inside"><?php print $content['4']; ?></div>
+	  </div>
   </div>
   <div class="homepage_wrapper clear">
 	  <div class="panel-panel grid-3">
@@ -42,13 +51,17 @@
 	    <div class="inside"><?php print $content['8']; ?></div>
 	  </div>
   </div>
-  <div class="panel-panel grid-12">
-    <div class="inside"><?php print $content['11']; ?></div>
+  <div class="homepage_wrapper clear">
+	  <div class="panel-panel grid-12">
+	    <div class="inside"><?php print $content['11']; ?></div>
+	  </div>
   </div>
-  <div class="panel-panel grid-6">
-    <div class="inside"><?php print $content['12']; ?></div>
-  </div>
-  <div class="panel-panel grid-6">
-    <div class="inside"><?php print $content['13']; ?></div>
+  <div class="homepage_wrapper clear">
+	  <div class="panel-panel grid-6">
+	    <div class="inside"><?php print $content['12']; ?></div>
+	  </div>
+	  <div class="panel-panel grid-6">
+	    <div class="inside"><?php print $content['13']; ?></div>
+	  </div>
   </div>
 </div>
