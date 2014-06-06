8654a879d1894f8e7544298a6ae0cdc75a11227a
Rik Barwick <rik@creativitea.co.uk>
2014-06-06 11:09:38 +0100

    add font awesome

--- public/sites/all/themes/communilabs/templates/html.tpl.php
+++ public/sites/all/themes/communilabs/templates/html.tpl.php
@@ -4,6 +4,7 @@
   <?php print $head; ?>
   <title><?php print $head_title; ?></title>  
   <?php print $styles; ?>
+  <link href="//netdna.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
   <?php print $scripts; ?>
   <!--[if lt IE 9]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
 </head>
